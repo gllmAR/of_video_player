4391ed79ec19626b4ed852f21de4e4d2f53e4725
Guillaume Arseneault <gllm@artificiel.org>
2018-04-07 14:38:30 -0400

    add gles native for raspberry

--- of_video_player/src/main.cpp
+++ of_video_player/src/main.cpp
@@ -5,7 +5,7 @@
 int main( ){
     //ofSetupOpenGL(1024,768,OF_WINDOW);
     ofGLESWindowSettings settings;
-    settings.setGLESVersion(2);
+    settings.glesVersion = 2;
     ofCreateWindow(settings);
     ofRunApp(new ofApp());
 };
